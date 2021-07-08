5a38bbe6f76cf0f6ce51d2ee1df59d0f2fde9211
sugar <sugar@DESKTOP-AVOGRMG>
2021-07-08 12:32:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+	{
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 25, 86, 495, 41 };
+            int length = array.Length;
+
+            for (int i = 0; i < length; i++)
+                Console.Write(array[i] + " ");
+
+
+            Console.WriteLine();
+            Console.WriteLine(length);
+            Console.ReadKey();
+
+
+        }
+	}
+}
